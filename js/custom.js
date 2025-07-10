d1e1e716f9971a28d38c233d4edc2dc2228aaca2
Alper Uyanik <alperuyanik4342@gmail.com>
2025-07-10 21:16:25 +0300

    hamburger color fixed

--- js/custom.js
+++ js/custom.js
@@ -3,16 +3,20 @@ window.addEventListener("load", function() {
         let navbar = document.getElementById("navbarCustom");
         let menuItems = navbar.getElementsByClassName("menu-item");
         let btnMain = document.querySelectorAll('.btn-main');
+        let hamburger = document.querySelector('#menu-btn');
 
         let primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary-color').trim();
 
         if (window.scrollY > 50) {
+            // Navbar background
             navbar.style.backgroundColor = "white";
 
+            // Menu items text color
             for (let item of menuItems) {
                 item.style.color = primaryColor;
             }
 
+            // Buttons
             btnMain.forEach(function(button) {
                 button.style.backgroundColor = "white";
                 button.style.color = primaryColor;
@@ -28,13 +32,30 @@ window.addEventListener("load", function() {
                     button.style.color = primaryColor;
                 };
             });
+
+            // Hamburger menu button
+            if (hamburger) {
+                hamburger.style.color = primaryColor;
+                hamburger.onmouseover = function() {
+                    hamburger.style.color = "white";
+                    hamburger.style.backgroundColor = primaryColor;
+                };
+                hamburger.onmouseout = function() {
+                    hamburger.style.color = primaryColor;
+                    hamburger.style.backgroundColor = "transparent";
+                };
+            }
+
         } else {
+            // Navbar background
             navbar.style.backgroundColor = primaryColor;
 
+            // Menu items text color
             for (let item of menuItems) {
                 item.style.color = "white";
             }
 
+            // Buttons
             btnMain.forEach(function(button) {
                 button.style.backgroundColor = primaryColor;
                 button.style.color = "white";
@@ -50,6 +71,19 @@ window.addEventListener("load", function() {
                     button.style.color = "white";
                 };
             });
+
+            // Hamburger menu button
+            if (hamburger) {
+                hamburger.style.color = "white";
+                hamburger.onmouseover = function() {
+                    hamburger.style.color = primaryColor;
+                    hamburger.style.backgroundColor = "white";
+                };
+                hamburger.onmouseout = function() {
+                    hamburger.style.color = "white";
+                    hamburger.style.backgroundColor = "transparent";
+                };
+            }
         }
     });
 });
